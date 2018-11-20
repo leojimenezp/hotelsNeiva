20cdc544fc9b7245e29b89034ec607e3ce870ab0
leonardo <44080750+leojimenezp@users.noreply.github.com>
2018-11-19 19:28:11 -0500

    apps
    .

---
+++ hotelneiva.sql
@@ -0,0 +1,53 @@
+/*
+SQLyog Community v8.71 
+MySQL - 5.5.5-10.1.25-MariaDB : Database - hotelesneiva
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hotelesneiva` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `hotelesneiva`;
+
+/*Table structure for table `roles` */
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_rol` varchar(50) DEFAULT NULL,
+  `descri` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `roles` */
+
+insert  into `roles`(`id`,`nombre_rol`,`descri`) values (1,'Admin','administrador'),(2,'cliente','cliente del hotel'),(3,'camarero','persona que atiende los servicios');
+
+/*Table structure for table `usuario` */
+
+DROP TABLE IF EXISTS `usuario`;
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(50) DEFAULT NULL,
+  `contrasena` varchar(50) DEFAULT NULL,
+  `rol` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*Data for the table `usuario` */
+
+insert  into `usuario`(`id`,`usuario`,`contrasena`,`rol`) values (4,'leonardo','1234',1),(5,'leonardo','1234',NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
